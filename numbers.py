1b1c6c1d793a1aecf13d8bc6ce089fa4212d4f59
aronfloppi <florin2p@gmail.com>
2021-12-26 19:09:55 +0100

    addes numbers.py

---
+++ numbers.py
@@ -0,0 +1,19 @@
+# Numbers in Python can be of 3 types: int , float and complex
+
+# Integer numbers
+# Integer numbers are represented using the int class
+
+age = 8
+age = int(8)
+type(age) == int
+# True
+
+# Floating point numbers
+# Floating point numbers (fractions) are of type float
+
+fraction = 0.1
+fraction = float(0.1)
+type(fraction) == float
+
+# Complex numbers
+# Complex numbers are of type complex
