1615190a3413ff4c04cd4e2d338f13aaf6dd9a60
arronfloppy <florin2p@gmail.com>
2024-08-14 18:45:13 +0200

    modified:   date_time.py

--- date_time.py
+++ date_time.py
@@ -1,4 +1,12 @@
 import time
+from datetime import date, datetime, timedelta, timezone
 
 time1 = time.time()
 print(str(time1))
+
+
+print(str(datetime.now(timezone.utc)))
+dt1 = datetime.fromisoformat('2024-08-14 18:10:23+02:00')
+dt2 = datetime.now(timezone.utc)
+
+print(str(dt2-dt1))
